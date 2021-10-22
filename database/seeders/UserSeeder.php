4580d67f6823fea5dee2ee65afc5a60b097ae424
Vahid Ramezani Pour <vahidramezanipour@gmail.com>
2021-10-22 18:56:26 +0330

    Improved Seeders classes and added UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,6 +14,42 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $users = [
+            [
+                'name'     => 'Flashbox Administrator',
+                'email'    => 'admin@flashbox.com',
+                'password' => 'admin',
+                'roles'    => ['admin']
+            ],
+            [
+                'name'     => 'Moein Pakkhesal',
+                'email'    => 'moein@flashbox.com',
+                'password' => 'moein',
+                'roles'    => ['seller']
+            ],
+            [
+                'name'     => 'Vahid Ramezanipour',
+                'email'    => 'vahid@flashbox.com',
+                'password' => 'vahid',
+                'roles'    => ['customer']
+            ]
+        ];
+
+        $existedUsers = User::whereIn('email', array_map(fn($u) => $u['email'], $users))->pluck('email')->toArray();
+
+        foreach($users as $userData){
+            if(!in_array($userData['email'] , $existedUsers)){
+                $existedUsers[] = $userData['email'];
+                $roles = $userData['roles'];
+                unset($userData['roles']);
+
+                $userData['password'] = \Illuminate\Support\Facades\Hash::make($userData['password']);
+                $user = \App\Models\User::firstOrCreate($userData);
+
+                $rolesId = \App\Models\Role::whereIn('identity', $roles)->pluck('id')->toArray();
+                $user->roles()->sync($rolesId);
+            }
+        }
+
     }
 }

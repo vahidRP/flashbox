f6f6b2ee96095609ab5d16116c49ea6d0f27d9f4
Vahid Ramezani Pour <vahidramezanipour@gmail.com>
2021-10-22 19:26:05 +0330

    Implemented all models policies

--- app/Policies/ReceiptPolicy.php
+++ app/Policies/ReceiptPolicy.php
@@ -3,80 +3,8 @@
 namespace App\Policies;
 
 use App\Models\Receipt;
-use App\Models\User;
-use Illuminate\Auth\Access\HandlesAuthorization;
 
-class ReceiptPolicy
+class ReceiptPolicy extends Policy
 {
-    use HandlesAuthorization;
-
-    /**
-     * authorize all actions within a given policy
-     *
-     * @param $user
-     * @param $ability
-     * @return bool
-     */
-    public function before($user, $ability)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view any receipts.
-     *
-     * @param  User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the receipt.
-     *
-     * @param  User  $user
-     * @param  Receipt  $receipt
-     * @return mixed
-     */
-    public function view(User $user, Receipt $receipt)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create receipts.
-     *
-     * @param  User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the receipt.
-     *
-     * @param  User  $user
-     * @param  Receipt  $receipt
-     * @return mixed
-     */
-    public function update(User $user, Receipt $receipt)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the receipt.
-     *
-     * @param  User  $user
-     * @param  Receipt  $receipt
-     * @return mixed
-     */
-    public function delete(User $user, Receipt $receipt)
-    {
-        //
-    }
+    protected string $model = Receipt::class;
 }
--- app/Policies/StorePolicy.php
+++ app/Policies/StorePolicy.php
@@ -3,80 +3,8 @@
 namespace App\Policies;
 
 use App\Models\Store;
-use App\Models\User;
-use Illuminate\Auth\Access\HandlesAuthorization;
 
-class StorePolicy
+class StorePolicy extends Policy
 {
-    use HandlesAuthorization;
-
-    /**
-     * authorize all actions within a given policy
-     *
-     * @param $user
-     * @param $ability
-     * @return bool
-     */
-    public function before($user, $ability)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view any stores.
-     *
-     * @param  User   $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the store.
-     *
-     * @param  User   $user
-     * @param  Store   $store
-     * @return mixed
-     */
-    public function view(User $user, Store $store)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create stores.
-     *
-     * @param  User   $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the store.
-     *
-     * @param  User   $user
-     * @param  Store   $store
-     * @return mixed
-     */
-    public function update(User $user, Store $store)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the store.
-     *
-     * @param  User   $user
-     * @param  Store   $store
-     * @return mixed
-     */
-    public function delete(User $user, Store $store)
-    {
-        //
-    }
+    protected string $model = Store::class;
 }

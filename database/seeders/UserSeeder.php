4816d9a65b02b8f35caecea4cab8e0c82f1e6cad
Vahid Ramezani Pour <vahidramezanipour@gmail.com>
2021-10-22 18:31:18 +0330

    Seeders classes are ready now

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,10 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call([
-             PermissionSeeder::class,
-             RoleSeeder::class,
-             UserSeeder::class,
-         ]);
+        //
     }
 }
